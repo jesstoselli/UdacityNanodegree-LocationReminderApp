920c60172dcd3407c9342fd5c2f48d700459c589
Jessyca Toselli <jfamedeiros@gmail.com>
2022-01-23 21:32:23 -0300

    Authentication working

--- app/src/main/java/com/udacity/project4/MyApp.kt
+++ app/src/main/java/com/udacity/project4/MyApp.kt
@@ -1,7 +1,6 @@
 package com.udacity.project4
 
 import android.app.Application
-import com.udacity.project4.authentication.AuthenticationViewModel
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
@@ -35,9 +34,6 @@ class MyApp : Application() {
                     get(),
                     get() as ReminderDataSource
                 )
-            }
-            single {
-                AuthenticationViewModel(get())
             }
             single { RemindersLocalRepository(get()) as ReminderDataSource }
             single { LocalDB.createRemindersDao(this@MyApp) }
